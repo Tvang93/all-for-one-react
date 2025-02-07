57516ecedbbf09e130f4783f24842c688fb4db6d
Tvang93 <vangt39@gmail.com>
2025-02-07 05:31:12 -0800

    NavBar and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { Button } from 'flowbite-react'
-import './App.css'
+import { Button } from "flowbite-react";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div className='border-8 bg-white text-black'>
-        <h1>Hello World</h1>
-      </div>
+      <section className="mt-60 lg:mt-80 bg-[url(../assets/img/pipboy-man.png)] bg-no-repeat bg-center bg-auto lg:bg-none">
+        <div className="2xl:text-9xl lg:text-8xl sm:text-5xl text-center text-[#00FF2C]">
+          <h1 className="pt-80 lg:pt-0">All For One</h1>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar1Component.jsx
+++ src/components/Navbar1Component.jsx
@@ -1,106 +1,130 @@
-import houseIcon from '../assets/house.svg'
-import hamburgerIcon from '../assets/bars-solid.svg'
-import arrowIcon from '../assets/arrow-right-solid.svg'
-import { NavLink } from 'react-router-dom';
-
+import { useEffect, useState } from "react";
+import homeIcon from "../assets/home.png";
+import hamburgerIcon from "../assets/more.png";
+import arrowIcon from "../assets/next.png";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar1Component = () => {
+  const [isNext, setIsNext] = useState(true);
+
+  const Next = (e) => {
+    if(isNext){
+      e.target.className = "rotate-180";
+    }else{
+      e.target.className = "";
+    }
+    setIsNext(!isNext);
+  };
+
+
   return (
-    <header className="flex">
+    <header className="flex justify-between lg:justify-normal">
       <div>
         <button
           id="dropdownMenuIconButton"
           data-dropdown-toggle="dropdownDots"
-          className="inline-flex pt-[28px] pl-[20px] p-2 rounded-lg lg:hidden"
+          className="inline-flex rounded-lg lg:hidden w-12"
           type="button"
         >
-          <img src={hamburgerIcon} alt="hamburger dropdown icon"/>
+          <img src={hamburgerIcon} alt="hamburger dropdown icon" />
         </button>
       </div>
-      <div className="pt-[20px] pl-[20px] pr-[20px] lg:pr-0 lg:border-b-1 border-b-[#00FF2C] grow-0 shrink-0">
-          <img src={houseIcon} alt="house icon"/>
-      </div>
-      <div className="grow-1 lg:flex hidden">
-        <div className="border-b-[#00FF2C] border-b-1 grow-1"></div>
-        <NavLink
-          to='./HelloWorld'
-          id="helloWorldLink"
-          className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10"
-        >
-        Hello World
-        </NavLink>
-        <NavLink
-        to='./Magic8Ball'
-          id="magic8BallLink"
-          className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10 hidden"
-        >
-        Magic 8 Ball
-        </NavLink>
-        <div className="border-b-[#00FF2C] border-b-1 grow-1"></div>
-        <NavLink
-          to='./AskingQuestions'
-          id="askingQuestionsLink"
-          className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10"
-        >
-        Asking Questions
-        </NavLink>
-        <NavLink
-          to='./RestaurantPicker'
-          id="restaurantLink"
-          className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10 hidden"
-        >
-        Restaurant Picker
-        </NavLink>
-        <div className="border-b-[#00FF2C] border-b-1 grow-1"></div>
-        <NavLink
-          to='./Add2Numbers'
-          id="add2NumLink"
-          className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10"
-        >
-        Adding 2 Numbers
-        </NavLink>
-        <NavLink
-          to='./ReverseItAlphanumerical'
-          id="reverseItANLink"
-          className="font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10 hidden"
-        >
+      <Link className="lg:py-5 lg:pl-5 lg:border-b-2 border-b-[#00FF2C] flex-grow-0 shrink-0 w-12 lg:w-[70px]" to="/">
+        <img src={homeIcon} alt="hamburger dropdown icon" />
+      </Link>
+      {isNext ? (
+        <div className="flex-grow lg:flex hidden">
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/HelloWorld"
+            id="helloWorldLink"
+            className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
+            Hello World
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/AskingQuestions"
+            id="askingQuestionsLink"
+            className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
+            Asking Questions
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/Add2Numbers"
+            id="add2NumLink"
+            className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
+            Adding 2 Numbers
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/OddOrEven"
+            id="oddOrEvenLink"
+            className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
+            Odd Or Even
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/MadLib"
+            id="madLibLink"
+            className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
+            MadLib
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+        </div>
+      ) : (
+        <div className="flex-grow lg:flex hidden">
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/Magic8Ball"
+            id="magic8BallLink"
+            className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
+            Magic 8 Ball
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/RestaurantPicker"
+            id="restaurantLink"
+            className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
+            Restaurant Picker
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/ReverseItAlphanumerical"
+            id="reverseItANLink"
+            className="font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
             <h1 className="text-[14px]">Reverse It</h1>
             <h2 className="text-[10px]">(AlphaNumerical)</h2>
-        </NavLink>
-        <div className="border-b-[#00FF2C] border-b-1 grow-1"></div>
-        <NavLink
-          to='./OddOrEven'
-          id="oddOrEvenLink"
-          className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10"
-        >
-        Odd Or Even
-        </NavLink>
-        <NavLink
-          to='./ReverseItNumbersOnly'
-          id="reverseItNOLink"
-          className="font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10 hidden"
-        >
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/ReverseItNumbersOnly"
+            id="reverseItNOLink"
+            className="font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
             <h1 className="text-[14px]">Reverse It</h1>
             <h2 className="text-[10px]">(Numbers Only)</h2>
-        </NavLink>
-        <div className="border-b-[#00FF2C] border-b-1 grow-1"></div>
-        <NavLink
-          to='./MadLib'
-          id="madLibLink"
-          className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10"
-        >
-            MadLib
-        </NavLink>
-        <NavLink
-          to='./GreaterThanLessThan'
-          id="greaterLessThanLink"
-          className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 grow-0 mt-10 hidden"
-        >
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+          <NavLink
+            to="/GreaterThanLessThan"
+            id="greaterLessThanLink"
+            className="xl:text-2xl font-medium text-center text-[#00FF2C] border-2 border-[#00FF2C] border-b-0 flex-grow-0 mt-10 px-4 pt-2"
+          >
             Greater/Less Than
-        </NavLink>
-        <div className="border-b-[#00FF2C] border-b-1 grow-1"></div>
-      </div>
-      <div className="pt-[20px] pr-[20px] border-b-[#00FF2C] border-b-1 grow-0 shrink-0 lg:block hidden">
+          </NavLink>
+          <div className="border-b-[#00FF2C] border-b-2 flex-grow"></div>
+        </div>
+      )}
+
+      <div className="py-5 pr-5 border-b-[#00FF2C] border-b-2 flex-grow-0 shrink-0 lg:block hidden w-[70px]" onClick={Next}>
         <img src={arrowIcon} alt="arrow icon"/>
       </div>
     </header>
