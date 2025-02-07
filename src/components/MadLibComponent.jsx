95531902b943e023326683896e87b71191be15b7
Tvang93 <vangt39@gmail.com>
2025-02-07 01:18:47 -0800

    madlib

--- src/components/MadLibComponent.jsx
+++ src/components/MadLibComponent.jsx
@@ -1,9 +1,93 @@
-const MadLibComponent = () => {
-    return(
-        <>
-            <h1 className="text-white">Mad Lib</h1>
-        </>
-    )
-}
+import { useEffect, useState } from "react";
+import { GetMadLib } from "../services/services";
 
-export default MadLibComponent
+const MadLibComponent = () => {
+  const [entryArray, setEntryArray] = useState([]);
+  const [cpuResponse, setCpuResponse] = useState("");
+  const [counter, setCounter] = useState(0);
+  const [entryPrompts, setEntryPrompts] = useState("Type in an animal")
+
+  const entryPromtsArray = [
+    "Type in an animal",
+    "Type in a type of container",
+    "Type in your least favorite color",
+    "Type in a type of material that covers animals",
+    "Type in a verb or action",
+    "Type in your favorite material object or thing",
+    "Type in your favorite food",
+    "Type in something you want to do but never have",
+    "Type in a way to change your appearance",
+    "Type in your favorite color",
+  ];
+
+  const getEntries = (e) => {
+    if (e.target.value.trim(" ") != "") {
+      if (e.key === "Enter") {
+        setEntryArray((entry) => [...entry, e.target.value]);
+        setCounter(counter + 1);
+      }
+    }
+  };
+
+  useEffect(() => {
+    setEntryPrompts(entryPromtsArray[counter])
+  }, [counter])
+
+  useEffect(() => {
+    if (entryArray.length > 9) {
+      fetchMadLib();
+      console.log(entryArray);
+    }
+  }, [entryArray]);
+
+  const fetchMadLib = async () => {
+    setCpuResponse(
+      await GetMadLib(
+        entryArray[0],
+        entryArray[1],
+        entryArray[2],
+        entryArray[3],
+        entryArray[4],
+        entryArray[5],
+        entryArray[6],
+        entryArray[7],
+        entryArray[8],
+        entryArray[9]
+      )
+    );
+  };
+
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center px-15 py-45 sm:px-55 lg:py-0 lg:pt-20 lg:items-start lg:pl-50 2xl:pl-100 bg-[url(../assets/img/pipboy-man.png)] bg-no-repeat bg-center bg-[330px,430px] lg:bg-none">
+        {entryArray.length < 10 && (
+          <>
+            <ul className="text-[2.5rem] lg:text-[3.5rem] xl:text-[4rem] text-[#00FF2C] self-start">
+              <li id="pipboyResponse">PipBoy: {entryPrompts}.</li>
+            </ul>
+            <div
+              id="inputContainer"
+              className="flex flex-col text-[4rem] text-[#00FF2C] self-start"
+            >
+              <input
+                id="inputField"
+                className="inline border-1 border-[#00FF2C] text-[2rem] lg:text-[3.5rem] xl:text-[4rem] text-[#00FF2C] w-[300px] lg:w-[750px] xl:w-[940px] bg-transparent"
+                type="text"
+                placeholder="Please type in here."
+                onKeyDown={getEntries}
+              />
+            </div>
+          </>
+        )}
+
+        {cpuResponse !== "" && (
+          <div className="text-lg text-[#00FF2C] self-start">
+            <p>PipBoy: {cpuResponse}</p>
+          </div>
+        )}
+      </section>
+    </>
+  );
+};
+
+export default MadLibComponent;
--- src/components/RestaurantPickerComponent.jsx
+++ src/components/RestaurantPickerComponent.jsx
@@ -1,9 +1,36 @@
 const RestaurantPickerComponent = () => {
-    return(
-        <>
-            <h1 className="text-white">Restaurant Picker</h1>
-        </>
-    )
-}
+    
+  return (
+    <>
+      <section class="flex flex-col items-center justify-center px-15 md:px-55 lg:py-0 lg:pt-20 lg:items-start lg:pl-50 2xl:pl-40 2xl:pr-90 bg-[url(../assets/img/pipboy-man.png)] bg-no-repeat bg-center bg-[330px,430px] lg:bg-none">
+        <ul class="text-[2.5rem] lg:text-[3.5rem] xl:text-[4rem] text-[#00FF2C] self-start">
+          <li id="pipboyResponse">
+            PipBoy: Are you hungry? What do you feel like eating?
+          </li>
+        </ul>
+        <div id="optionsContainer" class="flex flex-col self-start">
+          <button
+            id="option1"
+            class="inline border-1 border-[#00FF2C] text-[2rem] text-[#00FF2C] w-[300px] lg:w-[620px] xl:w-[800px] 2xl:w-[940px] p-2 bg-[rgb(0,255,44,0.3)] rounded-[30px] mt-7 text-start pl-5"
+          >
+            Chicken
+          </button>
+          <button
+            id="option2"
+            class="inline border-1 border-[#00FF2C] text-[2rem] text-[#00FF2C] 2xl:w-[940px] p-2 bg-[rgb(0,255,44,0.3)] rounded-[30px] mt-7 text-start pl-5"
+          >
+            Beef
+          </button>
+          <button
+            id="option3"
+            class="inline border-1 border-[#00FF2C] text-[2rem] text-[#00FF2C] 2xl:w-[940px] p-2 bg-[rgb(0,255,44,0.3)] rounded-[30px] mt-7 text-start pl-5"
+          >
+            Seafood
+          </button>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default RestaurantPickerComponent
+export default RestaurantPickerComponent;
